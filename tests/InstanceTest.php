78e1baf5763bbe8087be9ca3dcc1260594932df5
Ashley Clarke <me@ashleyclarke.me>
2015-05-31 13:34:33 +0200

    added more instance tests

--- tests/InstanceTest.php
+++ tests/InstanceTest.php
@@ -42,12 +42,64 @@ class InstanceTest extends TestCase {
         $image = 'ubuntu-14-04-x64';
 
         $response = $this->prophesize('Rackr\Cloud\Response');
-        $response->only('droplet')->willReturn([]);
+        $response->only('droplet')->willReturn();
 
         $this->gateway  ->post('droplets', [ 'body' => compact('name', 'size', 'region', 'image') ])
                         ->willReturn($response);
         ;
 
         $this->instance->create($name, $size, $region, $image);
+    }
+
+    /** @test */
+    public function it_gets_an_instances_info()
+    {
+        $response = $this->prophesize('Rackr\Cloud\Response');
+        $response->only('droplet')->willReturn();
+
+        $this->gateway->get('droplets/123')->willReturn($response);
+
+        $this->instance->info(123);
+    }
+
+    /** @test */
+    public function it_destroys_an_instance()
+    {
+        $this->gateway->delete('droplets/123');
+
+        $this->instance->destroy(123);
+    }
+
+    /** @test */
+    public function it_gets_all_instances()
+    {
+        $response = $this->prophesize('Rackr\Cloud\Response');
+        $response->only('droplets')->willReturn();
+
+        $this->gateway->get('droplets')->willReturn($response);
+
+        $this->instance->all();
+    }
+
+    /** @test */
+    public function it_powers_on_an_an_instance()
+    {
+        $response = $this->prophesize('Rackr\Cloud\Response');
+        $response->only('action')->willReturn();
+
+        $this->gateway->post('droplets/123/actions', ['body' => ['type' => 'power_on']])->willReturn($response);
+
+        $this->instance->on(123);
+    }
+
+    /** @test */
+    public function it_powers_off_an_an_instance()
+    {
+        $response = $this->prophesize('Rackr\Cloud\Response');
+        $response->only('action')->willReturn();
+
+        $this->gateway->post('droplets/123/actions', ['body' => ['type' => 'power_off']])->willReturn($response);
+
+        $this->instance->off(123);
     }
 }
